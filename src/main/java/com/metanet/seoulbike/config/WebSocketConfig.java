11ce24ed48a8b5b89fee80182601b08b0bcad397
hyojun jeong <hyojun000404@gmail.com>
2026-04-08 15:13:27 +0900

    웹소켓 작업중

---
+++ src/main/java/com/metanet/seoulbike/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.metanet.seoulbike.config;
+
+import com.metanet.seoulbike.websocket.handler.MyWebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final MyWebSocketHandler myWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myWebSocketHandler, "/ws/lotto")
+                .setAllowedOriginPatterns("*");
+    }
+}
