61f70c9d2e36f46ad7258e3cea386489f9e8a3bb
hojunsung <sunghojunaaa@naver.com>
2026-04-08 11:10:12 +0900

    Initial commit

---
+++ src/main/java/com/metanet/seoulbike/SeoulBikeAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.metanet.seoulbike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulBikeAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulBikeAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metanet/seoulbike/SeoulBikeAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metanet.seoulbike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulBikeAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
