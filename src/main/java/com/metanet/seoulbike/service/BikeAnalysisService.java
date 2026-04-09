dfd4be9c31256f0d59c44b7c2ca0bf39b2594934
hyojun jeong <hyojun000404@gmail.com>
2026-04-09 14:20:13 +0900

    사용자 dashboard 완성

--- src/main/java/com/metanet/seoulbike/service/BikeAnalysisService.java
+++ src/main/java/com/metanet/seoulbike/service/BikeAnalysisService.java
@@ -3,9 +3,11 @@ package com.metanet.seoulbike.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.metanet.seoulbike.dto.BikeAnalysisDto;
+import com.metanet.seoulbike.dto.DashboardSummaryDto;
 import com.metanet.seoulbike.mapper.SeoulBikeMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -16,7 +18,14 @@ public class BikeAnalysisService {
 
     private final SeoulBikeMapper seoulBikeMapper;
     
+    //@Cacheable("getAgeGroupUsage")
     public List<Map<String, Object>> getAgeGroupUsage(BikeAnalysisDto analysisDto) {
+    	System.out.println("🔥 DB 조회 발생");
         return seoulBikeMapper.getAgeGroupUsage(analysisDto);
+    }
+    
+    //@Cacheable("dashboardSummary")
+    public DashboardSummaryDto getDashboardSummary() {
+        return seoulBikeMapper.getDashboardSummary();
     }
 }
